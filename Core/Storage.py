a365d99602ce35465a9b7fbc2ed1fc38eacb5523
VrwDev <vrwtutor@gmail.com>
2026-02-22 09:15:52 +0700

    Initial Commits

---
+++ Core/Storage.py
@@ -0,0 +1,104 @@
+import sqlite3,os,sys
+
+class Storage():
+    def __init__(self):
+        super().__init__()
+        self.conn = self.getDB()
+        self.c = self.conn.cursor()
+        self.c.execute("""
+        CREATE TABLE IF NOT EXISTS item (
+            id TEXT PRIMARY KEY,
+            total INTEGER
+        )
+        """)
+
+        self.c.execute("""
+        CREATE TABLE IF NOT EXISTS borrow (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            tanggal_pinjam TEXT,
+            nama_peminjam TEXT,
+            key TEXT
+        )
+        """)
+
+        self.c.execute("""
+        CREATE TABLE IF NOT EXISTS borrowitem (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            peminjam_id INTEGER,
+            nama_item TEXT,
+            tanggal_pinjam TEXT,
+            tanggal_kembali TEXT,
+            amount INTEGER,
+            FOREIGN KEY (peminjam_id) REFERENCES borrow(id)
+        )
+        """)
+
+        self.conn.commit()
+        self.conn.close()
+        print("Succes Create DB")
+        
+    def is_frozen(self):
+        return getattr(sys, "frozen", False)
+
+    def getDB(self):
+        if self.is_frozen():
+            base = os.getenv("APPDATA")
+            app_dir = os.path.join(base, "VLauncher")
+        else:
+            app_dir = "./Core/DB/"
+
+        os.makedirs(app_dir, exist_ok=True)
+        connDir = os.path.join(app_dir, "DataBase.db")
+        conn = sqlite3.connect(connDir)
+        return conn
+    
+    def addItem(self, name, total):
+        self.conn = self.getDB()
+        c = self.conn.cursor()
+        c.execute("""
+            INSERT INTO item (id, total)
+            VALUES (?, ?)
+            ON CONFLICT(id)
+            DO UPDATE SET total = excluded.total
+        """,(name, total))
+
+        self.conn.commit()
+        self.conn.close()
+        print(f"Sukses menambah {name} dengan value {total}")
+
+    def getItem(self):
+        self.conn = self.getDB()
+        self.c = self.conn.cursor()
+
+        self.c.execute("""
+            SELECT id, total FROM item
+            ORDER BY id COLLATE NOCASE
+        """)
+
+        self.row = self.c.fetchall()
+        self.conn.close()
+        return self.row 
+    
+    def borrowItem(self, items, key, date, peminjam):
+        conn = self.getDB()
+        c = conn.cursor()
+
+        c.execute("""
+            INSERT INTO borrow (tanggal_pinjam, nama_peminjam, key)
+            VALUES (?, ?, ?)
+        """,(date, peminjam, key))
+
+        pid = c.lastrowid
+
+        for item, amount in items.items():
+            c.execute("""
+                UPDATE item SET total = total - ? WHERE id = ?
+            """, (amount, item))
+
+            c.execute("""
+                INSERT INTO borrowitem (peminjam_id, nama_item, tanggal_pinjam, amount)
+                VALUES (?, ?, ?, ?)
+            """, (pid, item, date, amount))
+
+        conn.commit()
+        conn.close()
---
+++ main.py
@@ -0,0 +1,166 @@
+import sys, os, string, random
+from PySide6.QtWidgets import (QApplication, QMainWindow, QTableWidgetItem, 
+QAbstractItemView, QStyledItemDelegate, QLineEdit, QMessageBox, QInputDialog
+, QDialog, QVBoxLayout)
+from PySide6.QtGui import QIntValidator
+from PySide6.QtUiTools import QUiLoader
+from PySide6.QtCore import QFile, Qt
+from Core import Storage
+from datetime import datetime
+
+class numericDelegate(QStyledItemDelegate):
+    def __init__(self, maxNum, row, col):
+        super().__init__()
+        self.maxNum = maxNum
+        self.col = col
+        self.row = row
+
+    def createEditor(self, parent, option, index):
+        editor = super().createEditor(parent, option, index)
+        #if index.row() == self.row and index.column() == self.col:
+        if isinstance(editor, QLineEdit):
+            validator = QIntValidator(0, self.maxNum, editor)
+            editor.setValidator(validator)
+
+        #if editor.hasAcceptableInput():
+        #    pass
+        #else:
+        #    QMessageBox.critical(None, "Error", f"Please Enter Number Between 0 - {self.maxNum}")
+
+        return editor
+
+class main(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        #Windows Setup
+        self.setWindowTitle("Aplikasi Inventaris")
+        self.setGeometry(100, 100, 800, 600)
+        
+        #File/DB
+        loader = QUiLoader()
+        file = QFile(self.resource_path("Ui/main.ui"))
+        file.open(QFile.ReadOnly)
+        self.ui = loader.load(file, self)
+        file.close()
+        self.db = Storage.Storage()
+
+        #Item Referencing
+        self.table = self.ui.findChild(type(self.ui.ItemSelect), "ItemSelect")
+        self.pjmbtn = self.ui.findChild(type(self.ui.PijamBtn), "PijamBtn")
+        self.rtnW = self.ui.findChild(type(self.ui.RtnWindow), "RtnWindow")
+
+        #button Setup
+        self.pjmbtn.clicked.connect(self.getSelectedItem)
+        self.rtnW.clicked.connect(self.rtnWindow)
+
+        #Post Setup
+        self.setCentralWidget(self.ui)
+        #self.db.addItem("test", "5")
+        self.loadItem()
+
+    def rtnWindow(self):
+        text, ok = QInputDialog.getText(self, "Masukkan Kode", "Masukkan Kode Pijammu")
+        if ok and text:
+            dlg = returnWindow(text)
+            dlg.exec()
+        else:
+            QMessageBox.critical(self, "Error", "Tolong Masukkan Kode Peminjaman")
+
+    def loadItem(self):
+        items = self.db.getItem()
+        self.table.setRowCount(len(items))
+        self.table.setEditTriggers(
+            QAbstractItemView.EditTrigger.DoubleClicked |
+            QAbstractItemView.EditTrigger.SelectedClicked
+        )
+
+        for row, (id, total) in enumerate(items):
+            item = QTableWidgetItem(str(id))
+            item.setCheckState(Qt.CheckState.Unchecked)
+            item.setFlags(item.flags() & ~Qt.ItemFlag.ItemIsEditable)
+            self.table.setItem(row, 2, item)
+
+            _total = QTableWidgetItem(str(total))
+            _total.setFlags(_total.flags() & ~Qt.ItemFlag.ItemIsEditable)
+            self.table.setItem(row, 0, _total)
+
+            editable_item = QTableWidgetItem("0")
+            editable_item.setFlags(editable_item.flags() | Qt.ItemFlag.ItemIsEditable)
+            self.table.setItem(row, 1, editable_item)
+            self.table.setItemDelegate(numericDelegate(total, row, 1))
+
+    def getSelectedItem(self):
+        date = datetime.now()
+        items = {}
+        error = False
+
+        text, ok = QInputDialog.getText(self, "Nama Peminjam", "Masukkan Nama Peminjam:")
+        if ok and text:
+            for row in range(self.table.rowCount()):
+                item = self.table.item(row, 0)
+                if item != None and int(item.text()) > 0:
+                    data = self.table.item(row, 2)
+                    if data.checkState() == Qt.CheckState.Checked:
+                        amnt = int(self.table.item(row, 1).text())
+                        if amnt > 0:
+                            items[data.text()] = amnt
+                        else:
+                            error = True
+
+            if len(items) > 0 and error == False:
+                key = self.randomKeyCode()
+                self.db.borrowItem(items, key, date, text)
+                QMessageBox.information(self, "Kode Pijam", f"Kode Pijammu Adalah '{key}' SIMPAN DENGAN BAIK")
+            elif error:
+                QMessageBox.critical(self, "Error", "Tolong Masukkan Jumlah Item Yang Ingin Dipijam")
+            else:
+                QMessageBox.critical(self, "Error", "Tolong Memilih Setidaknya 1 Item")
+        else:
+            QMessageBox.critical(self, "Error", "Tolong Masukkan Nama Peminjam")
+
+    def randomKeyCode(self):
+        chars = string.ascii_uppercase + string.digits
+        key = "".join([random.choice(chars) for _ in range(4)])
+
+        print(key)
+        return key
+
+    def resource_path(self, relative_path):
+        if hasattr(sys, "_MEIPASS"):
+            return os.path.join(sys._MEIPASS, relative_path)
+        return os.path.join(os.path.abspath("."), relative_path)
+
+class returnWindow(QDialog):
+    def __init__(self, code):
+        super().__init__()
+        self.code = code
+        #Windows Setup
+        self.setWindowTitle("Kembalikan Barang")
+        self.setGeometry(100, 100, 500, 400)
+        
+        #File/DB
+        loader = QUiLoader()
+        file = QFile(self.resource_path("Ui/return.ui"))
+        file.open(QFile.ReadOnly)
+        self.ui = loader.load(file, self)
+        file.close()
+        self.db = Storage.Storage()
+
+        #Item Referencing
+        self.table = self.ui.findChild(type(self.ui.ItemSelect), "ItemSelect")
+        self.RtnBtn = self.ui.findChild(type(self.ui.ReturnBtn), "ReturnBtn")
+
+        layouts = QVBoxLayout()
+        layouts.addWidget(self.ui)
+        self.setLayout(layouts)
+
+    def resource_path(self, relative_path):
+        if hasattr(sys, "_MEIPASS"):
+            return os.path.join(sys._MEIPASS, relative_path)
+        return os.path.join(os.path.abspath("."), relative_path)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = main()
+    window.show()
+    sys.exit(app.exec())
